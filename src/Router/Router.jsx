350b2d47294d1178a9749a05e7be786804237e05
TamannaTahsin1 <tamanntahsin648@gmail.com>
2024-01-13 11:27:28 +0600

    main layout created

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,17 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Homes/Home/Home";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children:[
+        {
+            path: "/",
+            element: <Home></Home>,
+        }
+    ]
+  },
+]);
 export default router;
